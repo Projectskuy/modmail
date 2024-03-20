456aea841667dc43c79369379e23c808bc75b90e
DenzzForTeams <96470753+Projectskuy@users.noreply.github.com>
2024-03-20 11:44:04 +0700

    Update example.config.js

--- example.config.js
+++ example.config.js
@@ -1,12 +1,12 @@
 module.exports = {
     client: {
         token: '', // ← Your bot token (.env IS RECOMMENDED)
-        id: '' // ← Your bot ID
+        id: '1214664524473311262' // ← Your bot ID
     },
     modmail: {
-        guildId: '', // ← Your server ID
-        categoryId: '', // ← The modmail category ID
-        staffRoles: [''], // ← The modmail staff roles IDs
+        guildId: '1219634544500670495', // ← Your server ID
+        categoryId: '1219634545029156924', // ← The modmail category ID
+        staffRoles: ['1219684494471331842'], // ← The modmail staff roles IDs
         mentionStaffRolesOnNewMail: true // ← Mention staff roles when there is a new mail?
     },
     logs: {
