d5df157c423018db5a7dff6179f0447464951456
DenzzForTeams <96470753+Projectskuy@users.noreply.github.com>
2024-03-21 06:02:16 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,15 @@
 module.exports = {
     client: {
         token: '', // ← Your bot token (.env IS RECOMMENDED)
-        id: '1214664524473311262' // ← Your bot ID
+        id: '1207745002650861623' // ← Your bot ID
     },
     modmail: {
-        guildId: '1219634544500670495', // ← Your server ID
-        categoryId: '1219634545029156924', // ← The modmail category ID
-        staffRoles: ['1219684494471331842'], // ← The modmail staff roles IDs
+        guildId: '1220087861274083399', // ← Your server ID
+        categoryId: '1220145047555805184', // ← The modmail category ID
+        staffRoles: ['1220087861290995778'], // ← The modmail staff roles IDs
         mentionStaffRolesOnNewMail: true // ← Mention staff roles when there is a new mail?
     },
     logs: {
-        webhookURL: '' // ← The logging webhook URL (OPTIONAL) (.env IS RECOMMENDED)
+        webhookURL: 'https://discord.com/api/webhooks/1220145361423695882/smcwFa-X6ea1E5-52mUMK1UaLp8_Kw1TB-hX0K4KgsT-8M1ltb2DsH91p8ek-bZMlZsE' // ← The logging webhook URL (OPTIONAL) (.env IS RECOMMENDED)
     }
 };
